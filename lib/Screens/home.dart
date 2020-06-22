e9ce0bc5e8e7b89901dba48645f8cc0fd6e0ad7d
kabagemark <kabagemark@gmail.com>
2020-06-22 14:51:00 +0300

    Firebase for database connected to the app

--- lib/Screens/home.dart
+++ lib/Screens/home.dart
@@ -11,7 +11,32 @@ class _HomeState extends State<Home> {
     return Container(
 
      child: Scaffold(
-      body:Center(child: Text('Home page'))
+      body:Padding(padding: EdgeInsets.fromLTRB(30, 30, 30, 0),
+      child: Column(
+           crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+              Center(
+              child:Text('notes', style:TextStyle(fontWeight: FontWeight.bold,fontSize: 30),)
+            ),
+             Divider(
+              height: 60,
+              color: Colors.grey[800],
+            ),
+          Card(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              const ListTile(
+                title: Text('This is an example of the notes taken'),
+                subtitle: Text('Here are the notes and you can click to read them'),
+              ),
+            ],
+          ),
+        )
+          ]
+      ),
+      )
+
      )
     );
   }
